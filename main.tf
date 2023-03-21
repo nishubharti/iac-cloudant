25cea03594e82d40959762d16ec50d63111d71eb
Nishu Bharti <nishu.bharti1@ibm.com>
2023-03-21 12:28:40 +0530

    changes name param

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "ibm_resource_group" "cloudant_group" {
 }
 
 resource "ibm_cloudant" "cloudant" {
-  name     = "test-cloudant"
+  name     = "test-cloudant1"
   location = var.ibm_region
   plan = var.plan
   resource_group_id = data.ibm_resource_group.cloudant_group.id
