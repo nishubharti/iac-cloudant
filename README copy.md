b47efe7522db657689369f28c6aa8b6d332f6681
Nishu Bharti <nishu.bharti1@ibm.com>
2023-03-21 10:46:25 +0530

    first commit

---
+++ README copy.md
@@ -0,0 +1,1 @@
+# iac cloudant template
---
+++ main.tf
@@ -0,0 +1,10 @@
+data "ibm_resource_group" "cloudant_group" {
+  name = var.resource_group
+}
+
+resource "ibm_cloudant" "cloudant" {
+  name     = "demo-cloudant"
+  location = var.ibm_region
+  plan = var.plan
+  resource_group_id = data.ibm_resource_group.cloudant_group.id
+}
